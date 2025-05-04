97f32a3294c042d2669d8ace3dd98a327e452222
bulat <bulatxajrutdinoff@yandex.ru>
2025-05-04 10:26:47 +0400

    Добавляет приложение challenges, меняет язык и таймзону

--- monthly_challenges/monthly_challenges/settings.py
+++ monthly_challenges/monthly_challenges/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'challenges.apps.ChallengesConfig'
 ]
 
 MIDDLEWARE = [
@@ -76,9 +77,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
